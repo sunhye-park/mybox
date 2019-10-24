0a5e93ef41743bd65e90211cad2c3d40a0d20a1c
kgitbank <kgitbank@DESKTOP-F24LFK3>
2019-10-24 14:34:32 +0900

    처음 올려봄!!

---
+++ Trygit/src/hellogit/Hellogit.java
@@ -0,0 +1,13 @@
+package hellogit;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Hellogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("mybox");
+	}
+
+}
